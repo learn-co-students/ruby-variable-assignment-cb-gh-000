86aefc96bbafb3b2773e168b208a819ba7aefe75
aviflombaum <avi@flombaum.com>
2015-09-20 17:55:00 -0400

    better local variable test

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -1,21 +1,10 @@
 # Code your solution in variable.rb
-describe "local variables" do
-
-  let(:right_answers) { ["greeting = \"Hello World\"", "greeting=\"Hello World\"", "greeting = \'Hello World\'", "greeting=\'Hello World\'"] }
-
-  before(:each) do
-    @content = File.open("variable.rb", "r") { |f| content = f.read }
-  end
+describe "./variable.rb" do
 
   it "defined a local variable called greeting and set it equal to 'Hello World'" do
-    expect(right_answers.any? { |answer| @content.match(answer) }).to eq(true)
+    greeting = get_variable_from_file('./variable.rb', "greeting")
+
+    expect(greeting).to eq("Hello World")
   end
 
-  it "should not be an instance variable" do
-    expect(@content).to_not include("@")
-  end
-
-  it "should not be an global variable" do
-    expect(@content).to_not include("$")
-  end
 end
--- variable.rb
+++ variable.rb
@@ -1,4 +1,2 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-
- 
