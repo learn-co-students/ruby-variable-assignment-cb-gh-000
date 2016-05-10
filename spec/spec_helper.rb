d4d146e9aacce99076d1510fe8c93c5eb62a465c
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-05-10 15:19:03 -0400

    fix broken string interpolation in spec helper raise name error'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,6 @@ def get_variable_from_file(file, variable)
   begin
     return file_scope.local_variable_get(variable)
   rescue NameError
-    raise NameError, "local variable `#{variable}' not defined in #{file}."
+    raise NameError, "local variable `#{variable}` not defined in #{file}."
   end
 end
