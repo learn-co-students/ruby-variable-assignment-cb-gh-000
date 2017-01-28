12cf6d7cd45a8ccc89e192c9bab0346f3dd49662
Suraj Kashyap <rzerin@wgu.edu>
2017-01-28 18:47:53 +0000

    Done.

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -1,10 +1,1 @@
-# Code your solution in variable.rb
-describe "./variable.rb" do
-
-  it "defined a local variable called greeting and set it equal to 'Hello World'" do
-    greeting = get_variable_from_file('./variable.rb', "greeting")
-
-    expect(greeting).to eq("Hello World")
-  end
-
-end
+greeting = "Hello World"
