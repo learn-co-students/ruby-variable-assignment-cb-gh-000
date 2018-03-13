57599208d4b1eb01a082806409fcb684915ab499
laura wall <lwall7722@gmail.com>
2018-03-13 21:21:47 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -7,3 +7,27 @@ student_1 = "Harry Potter"
 student_2 = "Ron Weasley"
 student_3 = "Hermione Granger"
 student_4 = "Draco Malfoy"
+
+students = ["Harry Potter", "Ron Weasley", "Hermione Granger", "Draco Malfoy"]
+
+puppies = ["bulldog", "terrier", "poodle"]
+
+random_numbers = [ 2, 5, 6, 8, 30050]
+
+mixed = ["this", "array", 7, 20, "has", 45, "integers", "&", "strings", 309]
+
+famous_cats =  ["Cheshire Cat", "Puss in Boots", "Garfield"]
+
+famous_cats[1]  #=> "Puss in Boots"
+
+famous_cats[0] #=> "Cheshire Cat"
+
+famous_cats[2] #=> "Garfield"
+
+famous_cats[20] #=> nil
+
+speed_dial = ["Ada", "Kay", "Matz", "DHH", "Borg"]
+speed_dial[1] #=> "Kay"
+
+speed_dial[1] = "Chipps"
+speed_dial[1] #=> "Chipps"
