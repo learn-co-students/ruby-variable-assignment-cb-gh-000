edf3a872f821d6919e9f32ee0eafbb548599b40a
laura wall <lwall7722@gmail.com>
2018-03-13 21:20:36 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -1,3 +1,9 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
  greeting = "Hello World"
+
+my_name = "Severus Snape"
+student_1 = "Harry Potter"
+student_2 = "Ron Weasley"
+student_3 = "Hermione Granger"
+student_4 = "Draco Malfoy"
