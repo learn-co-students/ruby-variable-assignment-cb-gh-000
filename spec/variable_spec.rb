b1ec05acd58c57325dcbfaef50b9e24086327769
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2018-09-24 12:19:53 +0000

    Done.

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -1,6 +1,5 @@
 # Code your solution in variable.rb
 describe "./variable.rb" do
-
   it "defined a local variable called greeting and set it equal to 'Hello World'" do
     greeting = get_variable_from_file('./variable.rb', "greeting")
 
